cf60b48eb216bb7912ecc6fbde21993a17f6977b
Mike Montpetit <mimontpe@cisco.com>
2024-04-03 12:45:23 -0400

    testing runner

---
+++ update-namespace-circleci.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+circleci admin import-orb circleci/github-cli@2.2.0
+circleci admin import-orb circleci/browser-tools@1.4.2
+circleci admin import-orb circleci/android@2.2.0
+circleci admin import-orb circleci/browser-tools@1.4.1
+circleci admin import-orb circleci/aws-cli@3.1.4
+circleci admin import-orb circleci/aws-ecs@3.2.0
+circleci admin import-orb circleci/aws-s3@4.0.0
+circleci admin import-orb circleci/bats@1.0.0
+circleci admin import-orb circleci/browser-tools@1.4.0
+circleci admin import-orb circleci/circleci-cli@0.1.9
+circleci admin import-orb circleci/continuation@0.3.1
+circleci admin import-orb circleci/gradle@3.0.0
+circleci admin import-orb circleci/jq@2.2.0
+circleci admin import-orb circleci/node@5.0.3
+circleci admin import-orb circleci/orb-tools@11.6.0
+circleci admin import-orb circleci/os-detect@0.3.0
+circleci admin import-orb circleci/python@2.1.1
+circleci admin import-orb circleci/ruby@2.0.0
+circleci admin import-orb circleci/rust@1.6.0
+circleci admin import-orb circleci/shellcheck@3.1.2
+circleci admin import-orb circleci/terraform@3.2.0
+circleci admin import-orb circleci/windows@5.0.0
+circleci admin import-orb circleci/circleci-cli@0.1.2
+circleci admin import-orb circleci/build-tools@3.0
+circleci admin import-orb circleci/os-detect@0.2
+circleci admin import-orb circleci/continuation@0.2.0
+circleci admin import-orb circleci/build-tools@3.0.0
+circleci admin import-orb circleci/docker@2.2.0
+circleci admin import-orb circleci/go@1.9.0
+circleci admin import-orb circleci/jira@1.3.1
+circleci admin import-orb circleci/path-filtering@0.1.3
+
+
